c07cbf4017f415b496088fafca1833583c1fe1f3
TokenWang <token.wang.chn@gmail.com>
2023-03-29 12:57:29 +0800

    pk

---
+++ web/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Navbar />
+  <router-view/>
+</template>
+
+<script>
+import Navbar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+import $ from "jquery";
+import { ref } from 'vue';
+export default {
+  components: {
+    Navbar
+  },
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_lv = ref("");
+    $.ajax({
+      
+
+    })
+
+    return {
+      bot_name,
+      bot_lv
+    }
+  }
+}
+
+</script>
+
+
+<style>
+body {
+  background-image: url(@/assets/images/background.jpg);
+  background-size: cover;
+}
+</style>
